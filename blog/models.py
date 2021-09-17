03c70b6e0fa1f25683caa659493765960cf4bfa7
Andre <andre.nogueira@nre.tec.br>
2021-09-17 14:40:33 -0300

    first commit

---
+++ blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name 
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    summary = RichTextField(blank=True, null=True)
+    body = RichTextUploadingField(blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
+    thumbnail = models.ImageField(verbose_name='Capa do Post', blank=True, null=True)
+    authorimage = models.ImageField (verbose_name='Foto do Autor', blank=True, null=True)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.title + '/' + str(self.author)
+
+
+
+
+
+
+
+
+        
---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include
+from django.urls import path
+from . import views
+from . views import HomeView, ArticleDetailView
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    #path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    path ('ckeditor/', include ('ckeditor_uploader.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
